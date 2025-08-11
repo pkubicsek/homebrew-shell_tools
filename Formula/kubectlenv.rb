97ba6cb3b83d45f84d1515c1a8219eef784fb123
Peter Kubicsek <dewil@hawk.local>
2025-08-11 12:03:29 +0200

    Update Formula to point to v1.1.0 tag with correct SHA256

--- Formula/kubectlenv.rb
+++ Formula/kubectlenv.rb
@@ -1,8 +1,8 @@
 class Kubectlenv < Formula
   desc "kubectl version manager inspired by tfenv"
   homepage "https://github.com/pkubicsek/homebrew-shell_tools"
-  url "https://github.com/pkubicsek/homebrew-shell_tools/archive/master.tar.gz"
-  sha256 "572c2b2bf0fd725235cbfeedc02ed0fb77151aaaf2b8d0413c5524417c202083"
+  url "https://github.com/pkubicsek/homebrew-shell_tools/archive/v1.1.0.tar.gz"
+  sha256 "cef7263c67988873baf8dfd85de353581dd3977f01fffa7654f54d78e105111f"
   license "MIT"
     version "1.1.0"
   head "https://github.com/pkubicsek/homebrew-shell_tools.git", branch: "master"
